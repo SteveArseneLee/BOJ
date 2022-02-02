56de46e45b69a7f6ad71d945d9116b6126d8b8c9
SteveArseneLee <lclgood@naver.com>
2022-02-02 20:44:53 +0900

    '220202'

--- Gold V/7576.py
+++ Gold V/7576.py
@@ -1,45 +1,45 @@
 from collections import deque
-from sys import stdin
 
-m, n = map(int, stdin.readline().split()) # m가로, n세로
+M, N = map(int, input().split())
+tots = [list(map(int, input().split())) for _ in range(N)]
+queue = deque() # 꼭 deque를 쓰지 않아도 됨
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, stdin.readline().split())))
+for i in range(N):
+    for j in range(M):
+        if tots[i][j] == 1:
+            queue.append([i, j])
+            
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-# print(graph)
+# 여기부터 bfs
+while queue:
+    row, col = queue.popleft()
+    
+    for k in range(4):
+        _row = row + dy[k]
+        _col = col + dx[k]
+        
+        # 안익은게 있다면, 익은 것으로 바꿔준다.
+        if 0 <= _row < N and 0 <= _col < M and tots[_row][_col] == 0:
+            tots[_row][_col] = tots[row][col] + 1
+            queue.append([_row, _col])
+# bfs 끝------
 
-# 상하좌우
-xh = [-1, 1, 0, 0]
-yw = [0, 0, -1, 1]
+# -1이 존재해서 -2 값으로 비교
+result = -2
+# 안익은게 있는지 체크
+check_tot = False
 
-def bfs():
-    while queue:
-        x, y = queue.popleft()
-        for d in range(4):
-            xx = xh[d] + x
-            yy = yw[d] + y
-            if 0 <= xx < n and 0 <= yy <m and graph[xx][yy] == 0:
-                graph[xx][yy] = graph[x][y] + 1
-                queue.append([xx, yy])
-
-queue = deque()
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            queue.append(([i,j])) # bfs함수 내부가 아닌 여기서 append를 해준다!!
-
-bfs()
-# print(graph) # bfs 수행 후 graph 점검
-
-result = -2 # 임의로 설정한 최소값
-flag = False # graph 탐색시 0인 값이 있다면 -1 출력위함
-for row in graph:
-    for r in row:
-        if r == 0:
-            flag = True
-        result = max(result, r) # 최대값
-
-if flag == True: print(-1)
-elif result == -1: print(0) # 모두 익은 경우 : 0출력
-else: print(result-1) # 결과 출력
+for i in tots:
+    for j in i:
+        if(j == 0):
+            check_tot = True
+        result = max(result, j)
+        
+if check_tot:
+    print(-1)
+elif result == -1:
+    print(0)
+else:
+    print(result - 1)
